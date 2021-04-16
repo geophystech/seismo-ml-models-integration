bfd32372b122530d688da0cf3c8d429d33c4811a
Vladimir <syler1983.9@gmail.com>
2021-04-16 16:55:19 +1000

    Update configs_run.py

--- scripts/configs_run.py
+++ scripts/configs_run.py
@@ -1,3 +1,21 @@
 import argparse
+import os
+import pathlib
 
+# Argument parsing
+# ../archive_scan.py ../test/ --reg config_*.ini --args "--start_time 14-04-2020 --end_time 15-04-2020"
 parser = argparse.ArgumentParser(formatter_class = argparse.RawTextHelpFormatter)
+
+parser.add_argument('run', help = 'Command to run. Better specified in quotes: "<run>"')
+parser.add_argument('configs_dir', help = 'Path to configs directory.')
+parser.add_argument('--reg', '--re', help = 'Regex for config files selection. Default: *.ini.')
+parser.add_argument('--args', '-a', help = 'Main script arguments. Specify in quotes: --args "<args>".')
+
+args = parser.parse_args()
+args = vars(args)
+
+print(f'WORK DIR: {pathlib.Path().absolute()}')
+
+files = os.listdir(args['configs_dir'])
+print(f'FILSE: {files}')
+
