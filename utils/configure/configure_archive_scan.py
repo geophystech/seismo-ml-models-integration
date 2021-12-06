3ef8b86dfe8add49b26c643a7761ba0e51bb9520
Vladimir <syler1983.9@gmail.com>
2021-12-06 22:43:19 +1000

    Update configure_archive_scan.py

--- utils/configure/configure_archive_scan.py
+++ utils/configure/configure_archive_scan.py
@@ -5,6 +5,27 @@ the archive_scan.py script run.
 import os
 
 
+def ask_yes_no(question, repeat=False):
+    """
+    Asks a question with answer YES/NO. Returns True if YES, False otherwise.
+    :param question - question to ask
+    :param repeat - if True, will repeat a question until either positive or negative answer given,
+        if False - any non-positive answer is treated as negative (no need to input exact negative answer, e.g.
+        "NO" or "N", etc.)
+    """
+    print(question + ' [Y/N]: ', end='')
+    answer = input()
+
+    while True:
+        answer = answer.strip().lower()
+        if answer in ['y', 'yes']:
+            return True
+        if not repeat:
+            return False
+        if answer in ['n', 'no']:
+            return False
+
+
 def ask(question, default=None, validation=None):
     if default:
         while True:
@@ -34,6 +55,10 @@ def configure_unix():
     default_database = os.environ.get('DEF_BASE')
     default_database = ask('Enter default database name (5 or shorter characters)',
                            default_database)
+
+    # Parse stations
+    from ..seisan import parse_seisan_def
+    seisan = os.path.join()
 
 
 def configure():
